0582380fa6f3abaf33a396847288f3c08cbfabfa
Vladimir Barakhtyanskiy <barahtasurgut@gmail.com>
2024-09-11 11:20:58 +0500

    qweqeqw

---
+++ client/src/pages/Main.js
@@ -0,0 +1,29 @@
+import style from './styles/Main.module.scss'
+import HeaderMain from "../components/header/HeaderMain";
+import AboutUs from "../components/about/AboutUs";
+import NewsBlock from "../components/news/NewsBlock";
+import Projects from "../components/projects/Projects";
+import NewHeader from "../components/newheader/NewHeader";
+import TwoBlocks from "../components/animation/TwoBlocks";
+import Group from "../components/groupcompany/Group";
+import NewAbout from "../components/newabout/NewAbout";
+import Actives from "../components/activeholding/Actives";
+import Footer from "../components/footer/Footer";
+import {useEffect} from "react";
+import {useLocation} from "react-router-dom";
+import Cosmos from "../components/cosmos/Cosmos";
+import Planet from "../components/planet/Planet";
+import Prices from "../components/prices/Prices";
+
+function Main () {
+
+
+    return (
+        <div className={style.bodymain}>
+            ads
+
+        </div>
+    )
+}
+
+export default Main
